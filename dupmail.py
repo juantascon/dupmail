8d2c5fcb2a311e7a65d97e7edfa8af0704f88668
Juan Tascon <juantascon@horlux.org>
2016-04-14 17:57:17 -0500

    adding code comments

--- dupmail.py
+++ dupmail.py
@@ -10,11 +10,21 @@ import sys
 import argparse
 import json
 
+
+#
+# Email Metadata representation
+#
 class Email:
+    #
+    # data is a dict containing every parsed field
+    #
     def __init__(self, id, data):
         self.id = id
         self.data = data
     
+    #
+    # Number of fields that are either empty or couldn't be parsed
+    #
     def fails(self):
         count = 0
         for value in self.data.values():
@@ -31,20 +41,32 @@ class Email:
     
     def __str__(self):
         return "|" + "|".join([str(self.data[key]) for key in sorted(self.data)]) + "|"
-        
     
     def hash(self):
         return hashlib.sha256(str(self).encode()).hexdigest()
 
+#
+# Parses 
+#
 class EmailParser:
     key_method_prefix = "process_"
     
+    #
+    # Parses an email and returns its metadata representation
+    #
     @staticmethod
     def parse(eml, keys, id):
         parser = EmailParser(eml, keys)
         email = Email(id, parser.data())
         return email
     
+    #
+    # A key is nothing but a parsable field, most are email headers such as:
+    # to, from, etc, others however, represent body information such as size
+    # or number of lines
+    # This functions lists available field parsers, ej: from, to, subject,
+    # body_size
+    #
     @classmethod
     def valid_keys(cls):
         prefix = cls.key_method_prefix
@@ -59,6 +81,9 @@ class EmailParser:
     def data(self):
         return self._data
     
+    #
+    # Dynamically calls any "process_" function
+    #
     def process(self, key):
         func = getattr(self, EmailParser.key_method_prefix+key)
         return func()
@@ -95,10 +120,22 @@ class EmailParser:
         [hashx.update(line.encode) for line in self.body()]
         return hashx.hexdigest()
     
+    #
+    # Converts/decodes email headers into string, it considers:
+    # internatialized headers such as: =?iso-8859-1?q?p=F6stal?=
+    # and utf-8/encoded headers such as: pöstal
+    #
     def parse_string(self, header, encoding="utf-8"):
+        #
+        # simple string headers
+        #
         if type(header) is str:
             return header
+        #
+        # encoded headers
+        #
         elif type(header) is bytes:
+            # tries to parse header with the given encoding, falls back to utf-8
             try:
                 return header.decode(encoding)
             except:
@@ -106,16 +143,29 @@ class EmailParser:
                     return self.parse_string(header, "utf-8")
                 else:
                     return ""
+        #
+        # internatialized headers
+        #
         elif type(header) is email.header.Header:
             value, header_encoding = email.header.decode_header(header)[0]
             return self.parse_string(value, header_encoding)
+        #
+        # invalid headers
+        #
         else:
-            raise TypeError("invalid type: %s %s"%(header, header.__classs__))
+            raise TypeError("invalid header: %s %s"%(header, header.__classs__))
     
+    #
+    # Parses a string and removes duplicate and trailing spaces/tabs/newlines
+    #
     def parse_string_flat(self, header):
         value = self.parse_string(header)
         return re.sub("\s\s+", " ", value.strip().lower())
     
+    #
+    # Removes realnames out of email addresses headers, it then lowercases the
+    # addresses and returns them sorted
+    #
     def parse_emails(self, headers):
         addresses = set()
         for header in headers:
@@ -124,18 +174,27 @@ class EmailParser:
                 addresses.add(addr.lower())
         return " ".join(sorted(addresses))
     
+    #
+    # Parses date headers fomatted as "yyyy-mm-dd"
+    #
     def parse_date(self, header):
         try:
             return email.utils.parsedate_to_datetime(header).strftime("%Y-%m-%d")
         except:
             return ""
     
+    #
+    # Body line iterator, removes spaces/tabs/newlines and skips empty lines
+    #
     def body(self):
         for line in email.iterators.body_line_iterator(self._eml):
             l = re.sub("\s", "", line)
             if len(l) > 0:
                 yield l
 
+#
+# Used to print email processing progress
+#
 class Progress:
     def __init__(self, obj, name):
         self.name = name
@@ -160,6 +219,9 @@ class EmailDups:
         self.skip_at = skip_at
         self.dups = {}
     
+    #
+    # Main process
+    #
     def calculate(self):
         dups = {}
         p = Progress(self.mbox, "emails")
@@ -176,6 +238,9 @@ class EmailDups:
             dups[xhash].append(emlhash)
         p.end()
         
+        #
+        # removes non duplicate emails
+        #
         for key in list(dups.keys()):
             if len(dups[key]) <= 1:
                 dups.pop(key, None)
@@ -209,7 +274,17 @@ parser.add_argument("-s", "--skip-if", default=2, type=int,
 parser.add_argument("-f", "--format", default="plain", choices=["plain", "json"],
                     help="print results using this format")
 parser.add_argument("-k", "--keys", nargs=1, default="from,to,date,subject,body_lines",
-                    help="comma separated list of field names used to identify duplicates")
+                    help="""
+comma separated list of fields to identify duplicates, valid values are:
+
+from: email address in From header
+to: ordered email address list including To, CC and BCC headers
+date: Date header formatted as yyyy-mm-dd, time is not included
+subject: lowercase, space removed Subject header
+body_lines: the number of non empty lines in the body, including attachments
+body_size: same as above but total byte size
+body_hash: same as above but sha256 representation
+""")
 parser.add_argument("path", help="maildir path")
 args = parser.parse_args()
 
